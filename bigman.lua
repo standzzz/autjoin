e4a23b1e77b48f7f8d3020900baff01b4c95a793
standzzz <stands903@gmail.com>
2024-12-30 22:27:23 +0000

    Add new entry to Lua table

--- bigman.lua
+++ bigman.lua
@@ -1,5 +1,6 @@
 local autojoins = {
     ["exampleUser"] = { ["id"] = 121323, ["channelid"] = 121212 },
+["exampleUser"] = { ["id"] = 121323, ["channelid"] = 121212 },
 {
     ["exampleUser"] = { ["id"] = 121323, ["channelid"] = 121212 },
 {
