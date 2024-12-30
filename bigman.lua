8b045b1abcd55c09a5157be53ad8a367d337d29e
standzzz <stands903@gmail.com>
2024-12-30 21:52:30 +0000

    Update bigman.lua

--- bigman.lua
+++ bigman.lua
@@ -1,13 +1,4 @@
 local autojoinlocal autojoins = {
-slocal table = {
-= {
     ["exampleUser"] = { ["id"] = 121323, ["channelid"] = 121212 },
 }
-    [exampleUser] = { [121323] = 121323, [121212] = 121212 },
-}
-
-return table
-    [exampleUser] = { [121323] = 121323, [121212] = 121212 },
-}
-
-return autojoinsreturn autojoins
+return autojoins
