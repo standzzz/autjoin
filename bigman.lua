7e75387b673ee5f46722c4d04563e770bd4ebb8e
standzzz <stands903@gmail.com>
2025-01-03 23:16:43 +0000

    Update bigman.lua

--- bigman.lua
+++ bigman.lua
@@ -1,5 +1,5 @@
 local autojoins = {
-    ["HardGroup9501"] = { ["id"] = 943650501, ["channelid"] = 121212 },
+    ["BubblyYolk7956"] = { ["id"] = 943650501, ["channelid"] = 121212 },
 ["exampleUser"] = { ["id"] = 121323, ["channelid"] = 121212 },
 ["exampleUser"] = { ["id"] = 121323, ["channelid"] = 121212 },
 ["exampleUser"] = { ["id"] = 121323, ["channelid"] = 121212 },
