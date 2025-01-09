e676d2a8f7d07130d33f4f1e4ccd1e4f5c0cfa4a
standzzz <stands903@gmail.com>
2025-01-09 02:46:36 +0000

    Update bigman.lua

--- bigman.lua
+++ bigman.lua
@@ -1,11 +1,11 @@
 local autojoins = {
 
-    ["JumpySpat9611"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["CelebratedPinkie7720"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["UsefulGinseng5232"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["AgreeableMobster2007"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["GuiltyOverflight5038"] = { ["id"] = 7411906033, ["channelid"] = 121212 },
-["BountifulAnklet2586"] = { ["id"] = 7411906033, ["channelid"] = 121212 },
+    ["JumpySpat9611"] = { ["id"] = 943650501, ["channelid"] = 121212 },
+["CelebratedPinkie7720"] = { ["id"] = 943650501, ["channelid"] = 121212 },
+["UsefulGinseng5232"] = { ["id"] = 943650501, ["channelid"] = 121212 },
+["AgreeableMobster2007"] = { ["id"] = 943650501, ["channelid"] = 121212 },
+["GuiltyOverflight5038"] = { ["id"] = 943650501, ["channelid"] = 121212 },
+["BountifulAnklet2586"] = { ["id"] = 943650501, ["channelid"] = 121212 },
 ["TestyBehold8307"] = { ["id"] = 943650501, ["channelid"] = 121212 },
 }
 
