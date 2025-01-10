c13f38cd36629cad7f2c04a1330d1ee36597d367
standzzz <stands903@gmail.com>
2025-01-10 18:19:13 +0000

    Update bigman.lua

--- bigman.lua
+++ bigman.lua
@@ -5,7 +5,7 @@ local autojoins = {
 ["UsefulGinseng5232"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
 ["AgreeableMobster2007"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
 ["GuiltyOverflight5038"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
-["BountifulAnklet2586"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
+["BlankNoise8318"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
 ["TestyBehold8307"] = { ["id"] = 7806083378, ["channelid"] = 121212 },
 ["GorgeousLongboat5130"] = { ["id"] = 7806083378, ["channelid"] = 121212 },
 ["OpulentFillet1130"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
