a6143d8b36bccebc388aef105ada8f7b2d5da5c3
standzzz <stands903@gmail.com>
2025-01-10 23:19:22 +0000

    Update bigman.lua

--- bigman.lua
+++ bigman.lua
@@ -3,17 +3,31 @@ local autojoins = {
     ["JumpySpat9611"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
 ["CelebratedPinkie7720"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
 ["UsefulGinseng5232"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
+["SpottedCarp2702"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
 ["AgreeableMobster2007"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["GuiltyOverflight5038"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
 ["BlankNoise8318"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["TestyBehold8307"] = { ["id"] = 7806083378, ["channelid"] = 121212 },
-["GorgeousLongboat5130"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
 ["OpulentFillet1130"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
 ["SlimAntler6601"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["SilentFlesh6432"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
-["ExemplaryTrooper1509"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
-    ["SpottedCarp2702"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-        ["RedAchievement6295"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
+ ["RedAchievement6295"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
+
+
+["TestyBehold8307"] = { ["id"] = 7806083378, ["channelid"] = 121212 },
+
+["GorgeousLongboat5130"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
+["GuiltyOverflight5038"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
+["reignbot213486"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
+["reignbot1534345"] = { ["id"] = 1321454129, ["channelid"] = 121212 },
+    
+
+        ["reignbot23454356"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
+     ["reignbot2831213"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
+     ["reignbot81235"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
+     ["reignbot03784"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
+
+     ["reignbot0327429"] = { ["id"] = 89592580, ["channelid"] = 121212 },
+     ["reignbot2134254"] = { ["id"] = 89592580, ["channelid"] = 121212 },
+     
+    
 }
 
 
