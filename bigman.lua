37ef62039004a5ebaa1a7d6508e7ddfa4c978e86
standzzz <stands903@gmail.com>
2025-01-10 16:28:29 +0000

    Update bigman.lua

--- bigman.lua
+++ bigman.lua
@@ -10,10 +10,10 @@ local autojoins = {
 ["GorgeousLongboat5130"] = { ["id"] = 7806083378, ["channelid"] = 121212 },
 ["OpulentFillet1130"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
 ["SlimAntler6601"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["SilentFlesh6432"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-["ExemplaryTrooper1509"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-    ["SpottedCarp2702"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
-        ["RedAchievement6295"] = { ["id"] = 2381828282, ["channelid"] = 121212 },
+["SilentFlesh6432"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
+["ExemplaryTrooper1509"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
+    ["SpottedCarp2702"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
+        ["RedAchievement6295"] = { ["id"] = 4331143961, ["channelid"] = 121212 },
 }
 
 
