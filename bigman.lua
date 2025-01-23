3c64ec5c4ed94ecba1b223b3977e5c742d774f10
standzzz <stands903@gmail.com>
2025-01-23 19:07:03 +0000

    Update bigman.lua

--- bigman.lua
+++ bigman.lua
@@ -128,10 +128,10 @@ local autojoins = {
      ["GloomyDemocracy3818"] = { ["id"] = 479135621, ["channelid"] = 28 },
     ["AncientOffice9522"] = { ["id"] = 5633053442, ["channelid"] = 28 },
 
-             ["reignsuperiority"] = { ["id"] = 5532804324, ["channelid"] = 29 },
-    ["reignwinslol"] = { ["id"] = 5532804324, ["channelid"] = 29 },
-     ["reignslapslol"] = { ["id"] = 5532804324, ["channelid"] = 29 },
-    ["reigndominateslol"] = { ["id"] = 5532804324, ["channelid"] = 29 },
+             ["reignsuperiority"] = { ["id"] = 4271114207, ["channelid"] = 29 },
+    ["reignwinslol"] = { ["id"] = 4271114207, ["channelid"] = 29 },
+     ["reignslapslol"] = { ["id"] = 4271114207, ["channelid"] = 29 },
+    ["reigndominateslol"] = { ["id"] = 4271114207, ["channelid"] = 29 },
      
 }
 
