691583ac65f5196f9d768c23b96709fd89e2070c
standzzz <stands903@gmail.com>
2025-02-04 19:49:38 +0000

    Update bigman.lua

--- bigman.lua
+++ bigman.lua
@@ -71,10 +71,10 @@ local autojoins = {
 ["reignbot048"] = { ["id"] = 6193458123, ["channelid"] = 9 },
 ["reignbot049"] = { ["id"] = 1873146655, ["channelid"] = 9 },
  
- ["reignbot787"] = { ["id"] = 1666857435, ["channelid"] = 25 },
- ["reignbot881"] = { ["id"] = 94328598, ["channelid"] = 25 },
- ["reignbot642"] = { ["id"] = 94328598, ["channelid"] = 25 },
- ["reignbot89"] = { ["id"] = 1666857435, ["channelid"] = 25 },
+ ["reignbot161"] = { ["id"] = 1666857435, ["channelid"] = 25 },
+ ["reignbot159"] = { ["id"] = 3274223668, ["channelid"] = 25 },
+ ["reignbot158"] = { ["id"] = 3274223668, ["channelid"] = 25 },
+ ["reignbot157"] = { ["id"] = 1666857435, ["channelid"] = 25 },
     
  ["reignbot1178"] = { ["id"] = 1873146655, ["channelid"] = 24 },
  ["reignbot116"] = { ["id"] = 2230361452, ["channelid"] = 24 },
